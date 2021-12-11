e3b7344e04a072abeab86590280b4a6388bfc41d
maxiemar <denishicmihail@gmail.com>
2021-12-11 18:41:08 +0300

    style: edit Appliances docs

--- src/main/java/by/bsuir/web/oop/entity/Appliances.java
+++ src/main/java/by/bsuir/web/oop/entity/Appliances.java
@@ -53,20 +53,6 @@ public class Appliances {
 
     /**
      * Gets the value of the appliances property.
-     *
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the appliances property.
-     *
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getAppliances().add(newItem);
-     * </pre>
-     *
-     *
      * <p>
      * Objects of the following type(s) are allowed in the list:
      * {@link Fridge },
